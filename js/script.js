2e94adcfe2bb528bcefd1de394b383e2dcdd3abe
Alex <alexwarchik@mail.ru>
2020-01-24 20:23:11 +0500

    lastv

---
+++ js/script.js
@@ -0,0 +1,42 @@
+var a = +prompt('Введите число');
+var b = +prompt('Введите степень');
+var c=1;
+
+if (isNaN(a)){
+    for (i = 0; i<b ;i++) {
+        c=c*5;       
+    }
+    alert(5 + ' ^ ' + b + ' = ' + c);
+}else if(isNaN(b)) {
+    for (i = 0; i<5 ;i++) {
+        c=c*a;       
+    }
+    alert(a + ' ^ ' + 5 + ' = ' + c);
+}else {
+    for (i = 0; i<b ;i++) {
+        c=c*a;       
+    }
+    alert(a + ' ^ ' + b + ' = ' + c);
+}
+
+
+
+do {
+    line = +prompt('Введите количество строк для лестницы');
+}while (line==0);
+if (isNaN(line)){
+    line = 5;
+}
+do {
+    var symbol = prompt('Введите символ отступов');
+}while (symbol==' ');
+
+do {
+    var symbol2 = prompt('Введите конечный символ');
+}while (symbol==' ');
+
+console.log(symbol2);
+for(i=1;i<line;i++){
+    symbol2=symbol+symbol2;
+    console.log(symbol2);
+}
